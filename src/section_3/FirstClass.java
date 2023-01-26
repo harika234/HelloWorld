7c434a785b271022c097293756bd7d6dacf3a9d9
harika234 <harikacdm@gmail.com>
2023-01-26 20:51:06 +0530

    Move files around

---
+++ src/section_3/FirstClass.java
@@ -0,0 +1,12 @@
+package section_3;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("section_3.data.Hello World");
+        System.out.println("section_3.data.Hello, Harika");
+
+    }
+
+
+
+}
--- src/section_3/SecondClass.java
+++ src/section_3/SecondClass.java
@@ -1,5 +1,7 @@
+package section_3;
+
 public class SecondClass {
     public static void main(String[] args) {
-        System.out.println("Hello Harika");
+        System.out.println("section_3.data.Hello Harika");
     }
 }
