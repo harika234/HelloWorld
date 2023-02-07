3ec78e6bb7cee31efabe15cf6bceeb359487523d
harika234 <harikacdm@gmail.com>
2023-02-07 06:36:50 +0530

    ClassesAndObjects

--- src/ControlFlow/MinAndMaxChallenge.java
+++ src/ControlFlow/MinAndMaxChallenge.java
@@ -1,4 +1,34 @@
 package ControlFlow;
 
+import java.util.Scanner;
 public class MinAndMaxChallenge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double max = 0;
+        double min = 0;
+        int loopCount=0;
+        while(true){
+            System.out.println("Enter a number, or any character to exit:");
+            String nextEntry = sc.nextLine();
+            try{
+               double validnum = Double.parseDouble(nextEntry);
+                if(loopCount ==0 || validnum<min){
+                    min = validnum;
+                }
+                if(loopCount ==0 || validnum>max){
+                    max = validnum;
+                }
+                loopCount++;
+            }catch (NumberFormatException nfe){
+                break;
+            }
+        }
+        if(loopCount>0){
+            System.out.println("min = "+min+", max = "+max);
+        }else{
+            System.out.println("No valid data entered");
+        }
+
+    }
+
 }
